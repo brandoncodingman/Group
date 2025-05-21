66c9653d7ec5e7e85165005a8b8106527f70d347
my_0123 <mturysm@gmail.com>
2025-05-21 12:08:03 +0900

    ログインページ作成

---
+++ Group/login.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/login.css">
+    <title>ログインページ</title>
+</head>
+
+<body>
+    <div class="login">
+        <p>ユーザーIDとパスワードを入力してください</p>
+        <div class="form">
+            <form action="" method="POST">
+                <div class="input">
+                    ユーザーID：<input type="text" name="username"><br>
+                    パスワード：<input type="text" name="password"><br>
+                </div>
+                <div class="submit"><input type="submit" value="ログイン" ></div>
+            </form>
+        </div>
+    </div>
+</body>
+
+</html>
