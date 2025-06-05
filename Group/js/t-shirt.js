47c6d13e21d6084119dc73a1fcb4a9fae74d6a44
my_0123 <mturysm@gmail.com>
2025-06-05 13:22:55 +0900

    ショップページ更新

--- Group/js/t-shirt.js
+++ Group/js/t-shirt.js
@@ -25,4 +25,42 @@ rightbutton.addEventListener('click', () => {
     }
 });
 
+//初期値設定
+let quantity = 1;
+
+const quantityElement = document.getElementById('quantity');
+const decreaseBtn = document.getElementById('decrease');
+const increaseBtn = document.getElementById('increase');
+
+decreaseBtn.addEventListener('click', () => {
+    if (quantity > 1) {
+        quantity--;
+        quantityElement.textContent = quantity;
+    }
+});
+
+increaseBtn.addEventListener('click', () => {
+    if (quantity < 10) {
+        quantity++;
+        quantityElement.textContent = quantity;
+    }
+});
+
+const addProduct = document.querySelector('input[type="submit"][value="カートに入れる"]');
+
+addProduct.addEventListener('click', () => {
+    const addProduct = {
+        name:"t-shirt",
+        price:3630,
+        quantity:quantity
+    };
+
+    let cart =JSON.parse(localStorage.getItem('cart')) || [];
+
+    cart.push(puroduct);
+
+    localStorage.setItem('cart',JSON.stringify(cart));
+
+    window.location.href = 'http://localhost/Group/Group/cart.php'
+});
 
