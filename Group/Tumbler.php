385c2629ac60e2618ed1851f4875d01daa5a02de
po_34 <ayako.ty.0604@docomo.ne.jp>
2025-06-06 15:49:26 +0900

    タンブラーページ修正

--- Group/Tumbler.php
+++ Group/Tumbler.php
@@ -74,7 +74,7 @@
         </div>
 
         <footer>&copy; 2025 Fluffy Planets <br><span class="iip">Created ❤️ with by Miyazaki, Matsura, Brandon.</span></footer>
-        <script src="./js/case_beige.js"></script>
+        <script src="./js/Tunbler.js"></script>
         <script src="./js/hamburger.js"></script>
         <script src="./js/shop.js"></script>
         <script src="./js/app.js"></script>
