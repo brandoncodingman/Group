dd3472da54e9fd6c8cccd2be2e0f03f6b9f6e336
my_0123 <mturysm@gmail.com>
2025-06-09 14:21:56 +0900

    cart更新

--- Group/Purchase history.php
+++ Group/Purchase history.php
@@ -1,18 +1,34 @@
 <?php
-require_once "./core/DbManager.php"
+require_once "./core/DbManager.php";
 
-try{
-    
+try {
+    $dbManager = new DbManager();
+    $pdo = $dbManager->getConnection();
+    // $userId = Session::getUserId();
+
+    $pdo->beginTransaction();
+    $stmt = $pdo->prepare('SELECT * FROM user_purchases ORDER BY date DESC');
+    $stmt->execute();
+    $result=$stmt->fetchAll(PDO::FETCH_ASSOC);
+    print_r($result);
+} catch (PDOException $e) {
+    die("エラーメッセージ:{$e->getMessage()}");
 }
+
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>注文履歴</title>
+    
+
 </head>
+
 <body>
 </body>
+
 </html>
